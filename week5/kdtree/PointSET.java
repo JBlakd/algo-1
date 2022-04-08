f2dab1d1a505ffad582f6b1a7b69d3f0ab020677
Ivan Hu <ivanhu2246@gmail.com>
2022-04-08 16:59:29 +0800

    Brute force testing

--- week5/kdtree/PointSET.java
+++ week5/kdtree/PointSET.java
@@ -4,15 +4,18 @@
  *  Description: Algos 1 kdtrees assignment
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.ArrayList;
 
 public class PointSET {
-    SET<Point2D> ps;
+    private SET<Point2D> ps;
 
     // construct an empty set of points
     public PointSET() {
@@ -44,7 +47,7 @@ public class PointSET {
     // draw all points to standard draw
     public void draw() {
         for (Point2D p : ps) {
-            StdDraw.point(p.x(), p.y());
+            p.draw();
         }
     }
 
@@ -83,6 +86,43 @@ public class PointSET {
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        PointSET brute = new PointSET();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            brute.insert(p);
+        }
 
+        // draw all of the points
+        StdDraw.clear();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        brute.draw();
+
+        RectHV myRect = new RectHV(0.11, 0.2, 0.57, 0.8);
+        StdDraw.setPenColor(StdDraw.PRINCETON_ORANGE);
+        StdDraw.setPenRadius(0.0025);
+        myRect.draw();
+
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius(0.01);
+        StdOut.println("The rectagle contains the following points:");
+        for (Point2D containedPoint : brute.range(myRect)) {
+            containedPoint.draw();
+            StdOut.println(containedPoint.toString());
+        }
+
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.setPenRadius(0.01);
+        Point2D randomPoint = new Point2D(StdRandom.uniform(0.0, 1.0), StdRandom.uniform(0.0, 1.0));
+        randomPoint.draw();
+        StdDraw.setPenRadius(0.005);
+        Point2D nearestPoint = brute.nearest(randomPoint);
+        randomPoint.drawTo(nearestPoint);
+        StdOut.println("The point nearest to " + randomPoint.toString() + " is " + nearestPoint
+                .toString());
     }
 }
